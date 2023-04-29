85a0426cf461b80d0478ff5478113e5ad38fa7c2
Eduardo <eduardoefb@gmail.com>
2023-04-29 18:43:49 -0300

    Mongodb update

--- images/init/wait_mongodb.sh
+++ images/init/wait_mongodb.sh
@@ -3,3 +3,6 @@ while ! curl ${PREFIX}-mongodb:27017; do
     echo "`date` Waiting for mongodb..."
     sleep 5
 done
+timer=`shuf -i 1-30 -n 1`
+echo "`date` MongoDB is ready. Waiting for ${timer} seconds to ensure its stability..."
+sleep ${timer}
